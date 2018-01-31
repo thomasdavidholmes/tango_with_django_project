e4fc2dcc87cd933d724560152e349a8469663b06
thomasdavidholmes <33161811+thomasdavidholmes@users.noreply.github.com>
2018-01-31 17:42:28 +0000

    Up to Ch7 Exs

--- rango/forms.py
+++ rango/forms.py
@@ -21,15 +21,29 @@ class PageForm(forms.ModelForm):
                          help_text="Please enter the url of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
-    class Meta:
-        # Provide an association between the ModelForm and a model
-        model = Page
 
-        # What fields do we want to include in our form?
-        # This way we don't need every field in the model present.
-        # Some fields may allow NULL values, so we may not want to include them...
-        # Here, we are hiding the foreign key.
-        # we can either exclude the category field from the form,
-        exclude = ('category',)
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+        cleaned_data['url'] = url
+
+        return cleaned_data
+
+
+class Meta:
+        # Provide an association between the ModelForm and a model
+    model = Page
+
+    # What fields do we want to include in our form?
+    # This way we don't need every field in the model present.
+    # Some fields may allow NULL values, so we may not want to include them...
+    # Here, we are hiding the foreign key.
+    # we can either exclude the category field from the form,
+    exclude = ('category',)
         #or specify the fields to include (i.e. not include the category field)
         #fields = ('title', 'url', 'views')
