747a231e04646cbf55f231ead0fecc126c346eed
thomasdavidholmes <33161811+thomasdavidholmes@users.noreply.github.com>
2018-02-02 19:37:10 +0000

    At 9 exs

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -27,4 +28,20 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    # Remember if you use Python 2.7.x, define __unicode__ too!
+
+    def __str__(self):
+        return self.user.username
+
 # Create your models here.
